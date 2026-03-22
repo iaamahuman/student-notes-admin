7ee7d2c545539edd50e3d39778cfe5145cb95e93
iaamahuman <iaamahuman@gmail.com>
2026-03-22 22:16:59 +0530

    Fix auth token for cross-domain

--- app/api/website/auth/user/route.ts
+++ app/api/website/auth/user/route.ts
@@ -1,23 +1,21 @@
 import { NextResponse } from "next/server";
 import { cookies } from "next/headers";
 import { verify } from "jsonwebtoken";
+
 export async function GET(req: Request) {
+  const authHeader = req.headers.get("Authorization");
+  const headerToken = authHeader?.replace("Bearer ", "");
   const cookieStore = cookies();
-  const token = cookieStore.get("token");
+  const cookieToken = cookieStore.get("token");
+  const token = headerToken || cookieToken?.value;
+
   if (!token) {
     return new NextResponse("Unauthorized User", { status: 200 });
   }
   try {
-    const data = verify(token.value, process.env.SECRET_KEY || "");
-    const response = new NextResponse(
-      JSON.stringify({
-        message: "Verified User",
-        user: data,
-      })
-    );
-    return response;
+    const data = verify(token, process.env.SECRET_KEY || "");
+    return new NextResponse(JSON.stringify({ message: "Verified User", user: data }));
   } catch (error) {
-    console.log(error);
     return new NextResponse("Internal Error", { status: 500 });
   }
 }
