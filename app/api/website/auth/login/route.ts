488bffdaeac94b3c2dda9ece2d60f5bfd2e576ce
iaamahuman <iaamahuman@gmail.com>
2026-03-22 22:52:36 +0530

    Return token in login response body

--- app/api/website/auth/login/route.ts
+++ app/api/website/auth/login/route.ts
@@ -42,6 +42,7 @@ export async function POST(req: Request) {
       JSON.stringify({
         message: "Successfully logged in",
         user: { ...modifiedUser },
+        token,
       })
     );
 
